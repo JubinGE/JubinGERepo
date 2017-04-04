f39db92c2211ad6939ac0e9333e3a2957d93a5a3
JubinGE <jubin.george@cognizant.com>
2017-04-04 14:40:49 +0530

    circleci project added

---
+++ app.js
@@ -0,0 +1,21 @@
+// Sample node.js web app for Pluralsight Docker CI course
+// For demonstration purposes only
+'use strict';
+
+var express = require('express'),
+    app = express();
+
+var port = process.env.PORT || 8080;
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+  });
+});
+
+app.listen(port, function(err){
+    console.log("server started on port" + port);
+});
+module.exports.getApp = app;
